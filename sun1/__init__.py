276fb6aa1a8a42f72b1088c95ae351bee7f6aa4f
Dublikata Studio <dimazikhih@gmail.com>
2026-09-07 23:37:05 +0300

    Initialize sun1 module with imports and version

---
+++ sun1/__init__.py
@@ -0,0 +1,7 @@
+from .client import Sun1
+from .translator import WeightsTranslator
+from .generator import DublikatAIGenerator
+from .server import app
+
+__version__ = "0.1.0"
+__all__ = ["Sun1", "WeightsTranslator", "DublikatAIGenerator", "app"]
