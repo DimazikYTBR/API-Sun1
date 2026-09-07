b95397e147da50caf2d5ca831a5e8fc2fd0fb6e2
Dublikata Studio <dimazikhih@gmail.com>
2026-09-07 23:53:58 +0300

    Rename Sun1 to APISun1 in module exports

--- sun1/__init__.py
+++ sun1/__init__.py
@@ -1,7 +1,7 @@
-from .client import Sun1
+from .client import APISun1
 from .translator import WeightsTranslator
 from .generator import DublikatAIGenerator
 from .server import app
 
 __version__ = "0.1.0"
-__all__ = ["Sun1", "WeightsTranslator", "DublikatAIGenerator", "app"]
+__all__ = ["APISun1", "WeightsTranslator", "DublikatAIGenerator", "app"]
