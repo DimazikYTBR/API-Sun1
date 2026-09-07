478208b1789b41a237a6a965086cefd158537b0f
Dublikata Studio <dimazikhih@gmail.com>
2026-09-08 00:01:04 +0300

    Fix typo in generator import and __all__ declaration

--- sun1/__init__.py
+++ sun1/__init__.py
@@ -1,7 +1,7 @@
 from .client import APISun1
 from .translator import WeightsTranslator
-from .generator import DublikatAIGenerator
+from .generator import DublikataAIGenerator
 from .server import app
 
 __version__ = "0.1.0"
-__all__ = ["APISun1", "WeightsTranslator", "DublikatAIGenerator", "app"]
+__all__ = ["APISun1", "WeightsTranslator", "DublikataAIGenerator", "app"]
