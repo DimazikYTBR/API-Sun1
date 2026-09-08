4a384887e24f6f5118340c395d6969687835dd1e
Dublikata Studio <dimazikhih@gmail.com>
2026-09-08 17:04:08 +0300

    Bump version from 0.1.0 to 0.1.1

--- sun1/__init__.py
+++ sun1/__init__.py
@@ -3,5 +3,5 @@ from .translator import WeightsTranslator
 from .generator import DublikataAIGenerator
 from .server import app
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["APISun1", "WeightsTranslator", "DublikataAIGenerator", "app"]
